139bf5b07ce51afa0bbdbd683a0b5c709db7c307
ntrongkhanh <33388614+ntrongkhanh@users.noreply.github.com>
2021-07-11 22:09:23 +0700

    a

--- src/main/java/com/weblaptop/backend/security/WebSecurityConfig.java
+++ src/main/java/com/weblaptop/backend/security/WebSecurityConfig.java
@@ -54,10 +54,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors()
-                .and()
-                .authorizeRequests()
-                .antMatchers("/**")
-                .permitAll()
                 .and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
